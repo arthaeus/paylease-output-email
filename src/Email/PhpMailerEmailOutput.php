1e840c0974f8d8b673b38fd523f2f95df107faa2
Your Name <you@example.com>
2016-07-20 16:43:10 +0000

    adding more dummy text to email

--- src/Email/PhpMailerEmailOutput.php
+++ src/Email/PhpMailerEmailOutput.php
@@ -27,5 +27,11 @@ class PhpMailerEmailOutput extends EmailOutput implements IOutput, IEmail , \SPL
 
     public function output( \stdClass $output )
     {
+        $settings      = \Calculator\DI\CalculatorProvider::config();
+        $this->emailer->addAddress($settings['email']['application']['EmailTo']);
+        $this->emailer->setFrom($settings['email']['application']['EmailFrom']);
+        $this->emailer->Subject = $settings['email']['application']['EmailSubject'];
+        $this->emailer->Body = $settings['email']['application']['EmailBody'];
+        echo " \n here the emailer would email "  . $this->emailer->Body . " value from src/config/application.ini \n";
     }
 }
