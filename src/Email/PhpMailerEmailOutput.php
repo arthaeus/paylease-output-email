9f1d9d02862b06ea0015b6f60707792e0e3c466f
Your Name <you@example.com>
2016-07-21 14:16:00 +0000

    cleanup

--- src/Email/PhpMailerEmailOutput.php
+++ src/Email/PhpMailerEmailOutput.php
@@ -5,26 +5,6 @@ namespace Output;
 class PhpMailerEmailOutput extends EmailOutput implements IOutput, IEmail , \SPLObserver
 {
 
-    protected $emailer = null;
-
-    public function getEmailer()
-    {
-        return $this->emailer;
-    }
-
-    public function setEmailer( $emailer )
-    {
-        $this->emailer = $emailer;
-    }
-
-    public function update( \SPLSubject $subject )
-    {
-        $message = $subject->getMessage();
-        $messageObj = new \stdClass();
-        $messageObj->message = $message;
-        $this->output( $messageObj );
-    }
-
     public function output( \stdClass $output )
     {
         $settings      = \Calculator\DI\CalculatorProvider::config();
